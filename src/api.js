aa351d1cbd8f216b9466febcc403aeb9a0ec070c
Shyam3050 <nimmanashyam3050@gmail.com>
2023-04-21 13:44:49 +0530

    updated

--- src/api.js
+++ src/api.js
@@ -1,24 +1,49 @@
-const express = require("express")
-const serverless = require("serverless-http")
+// This is your test secret API key.
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const serverless = require("serverless-http");
 
-const app = express()
-const router = express.Router()
+const router = express.Router();
 
+app.use(express.json());
+app.use(cors());
+
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+router.post("/create-checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: req.body.map((item) => {
+        return {
+          price_data: {
+            currency: "inr",
+            product_data: {
+              name: item.name,
+            },
+            unit_amount: item.unit_amount,
+          },
+          quantity: item.quantity,
+        };
+      }),
+      success_url: `${process.env.CLIENT_URL}/success`,
+      cancel_url: `${process.env.CLIENT_URL}/fail`,
+    });
+    res.status(200).send({ url: session.url });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
 
 router.get("/", (req, res) => {
-    res.json({
-        "hello" : "hi"
-    })
-})
+  res.json({
+    hello: "hi this is is stripe backend",
+  });
+});
 
-router.get("/test", (req,res) => {
-    res.json({
-        "test" : "test"
-    })
-})
+app.use(`/.netlify/functions/api`, router);
 
-app.use("/.netlify/functions/api", router)
-
-
-
-module.exports.handler = serverless(app)
+module.exports.handler = serverless(app);
